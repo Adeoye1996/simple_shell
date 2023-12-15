6d670ee466980c9b8f8669eab0f857b0f5319ac8
abdulazeez babatunde abdulrazaq <abdulazeezbabatunde10@gmail.com>
2023-12-15 15:15:05 -0800

    update

--- _atoi.c
+++ _atoi.c
@@ -1,10 +1,10 @@
 #include "shell.h"
 
 /**
- * interactive - checks if the shell is interactive mode
- * @info: struct containing relevant information
+ * interactive - checks if the shell is in interactive mode
+ * @info: struct address
  *
- * Return: 1 if interactive mode, 0 otherwise
+ * Return: 1 if in interactive mode, 0 otherwise
  */
 int interactive(info_t *info)
 {
@@ -12,32 +12,35 @@ int interactive(info_t *info)
 }
 
 /**
- * is_delim - checks if character is a delimiter
+ * is_delim - checks if a character is a delimiter
  * @c: the character to check
  * @delim: the delimiter string
  * Return: 1 if true, 0 if false
  */
-int is_delim(char c, char *delim)
+int is_delim(char c, const char *delim)
 {
 	while (*delim)
+
 		if (*delim++ == c)
+
 			return (1);
 	return (0);
 }
 
 /**
- * _isalpha -checks for an alphabetic character
+ * _isalpha - checks for an alphabetic character
  * @c: The character to check
  * Return: 1 if c is alphabetic, 0 otherwise
  */
 int _isalpha(int c)
 {
-	return ((c >= 'a' && c <= 'Z'));
+	return ((c >= 'a' && c <= 'z') || (c >= 'A' && c <= 'Z'));
 }
+
 /**
- * _atoi - coverts a string to an integer
+ * _atoi - converts a string to an integer
  * @s: the string to be converted
- * Return: 0 if no numbers in the string, converted number otherwise
+ * Return: 0 if no numbers in string, converted number otherwise
  */
 int _atoi(char *s)
 {
@@ -45,11 +48,13 @@ int _atoi(char *s)
 	unsigned int result = 0;
 
 	for (i = 0; s[i] != '\0' && flag != 2; i++)
+
 	{
-		if (s[i] == '_')
+		if (s[i] == '-')
 			sign *= -1;
 
 		if (s[i] >= '0' && s[i] <= '9')
+
 		{
 			flag = 1;
 			result *= 10;
@@ -58,7 +63,9 @@ int _atoi(char *s)
 		else if (flag == 1)
 			flag = 2;
 	}
+
 	output = (sign == -1) ? -result : result;
 
 	return (output);
 }
+
